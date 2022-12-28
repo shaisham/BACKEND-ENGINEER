e91fb4418d0611ae7b9d06b740071e1b4ed129b7
shaisham <102400953+shaisham@users.noreply.github.com>
2022-12-28 19:21:15 +0530

    Created a Money-Maker Project
    Money Maker
    
    In this project, I have used  C# to build a Money Maker: a program in which a user enters an amount and gets the minimum number of coins that equal that value.
    
    For example, 16 cents could be:
    
    16 bronze coins,
    3 silver coins and 1 bronze coin, or
    1 gold coin, 1 silver coin, 1 bronze coin
    
    This project will get you comfortable with division (/), rounding down (Math.Floor()), and modulo (%): we can find how many coins “fit” into an amount by dividing the amount by the coin value, rounding down, and finding the remainder.

---
+++ Money Maker/Money Maker/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Money_Maker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.WriteLine("Enter an amount to convert to coins:");
+            string str = Console.ReadLine();
+
+            //Convert the captured value (a string) to a number.
+            double amount = Convert.ToDouble(str);
+
+
+            Console.Write($"{amount} is equals to: \n");
+
+
+            //Define two variables that hold those values.
+            int goldValue = 10;
+            int silverValue = 5;
+
+            // Divide the amount by the value of a gold coin
+            // Round down to the nearest integer
+            // Store the result in a double variable called goldCoins
+
+            double goldcoins = Math.Floor(amount / goldValue);
+
+
+            //Use the modulo(%) operator to find the remaining amount and store it in a double variable.
+            double remainder = amount % goldValue;
+
+
+            //Find the maximum number of silver coins that “fit” into the remainder:
+            //Divide the remainder by the value of a silver coin
+            //Round down to the nearest integer
+            //Store the result in a double variable called silverCoins
+
+            double silverCoins = Math.Floor(remainder / silverValue);
+
+
+            //Use the modulo(%) operator to find the remaining amount and store it in a double variable.
+            remainder = remainder % silverValue;
+
+
+
+            // Print out all of the coins!
+            Console.WriteLine($"Gold coins: {goldcoins} \n");
+            Console.WriteLine($"Silver coins: {silverCoins} \n");
+            Console.WriteLine($"Bronze coins: {remainder}");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
