8fa39b864f7124f9eae6f7568e965d82d5d5259c
shaisham <102400953+shaisham@users.noreply.github.com>
2022-12-28 14:49:35 +0530

    Created a Console Creatures Project
    Created a ghost creature in a console application using parentheses () and tildes ~ in Visual Studio 2022.

---
+++ Console Creatures/Console Creatures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Creatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a ghost with the help of parentheses () and tildes ~.
+
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("His name is Shameeer");
+            Console.ReadLine();
+        }
+    }
+}
