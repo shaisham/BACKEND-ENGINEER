dc0187dc089d9bc4475f68c2120fda7e07c79311
shaisham <102400953+shaisham@users.noreply.github.com>
2023-01-14 15:10:47 +0530

    Created a Caesar Cipher Project Using C#
    By 6 a.m. on Sunday, your team’s project is nearly finished. The Object of Your Affection (the name’s a work in progress) is getting attention from other teams in the hackathon. With one day left, they’re getting desperate. To steal your project idea, your competitors have been reading your team’s emails!
    you have been asked to write a cipher: a tool to encrypt text, making it unreadable to other teams. You’ve decided to implement the Caesar Cipher, which was used by the Roman Empire to encode military secrets.
    
    To use the cipher, draw the alphabet in a circle like so:
    Take every letter of your message and shift it three places to the right. For example, A becomes D, B becomes E, C becomes F, and “attack” becomes “dwwdfn”.
    Your teammates can decrypt your message by reversing the process: shift each letter three places to the left.
    
    For this project you’ll need to convert strings to arrays of characters with ToCharArray():
    You’ll also need to convert arrays of characters to strings with Join():

---
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            //Prepare for Encryption
+            //ask the user for a secret message and store the result in a variable
+            Console.Write("Enter your secret Message\n");
+            string input = Console.ReadLine();
+
+
+
+            //Convert the captured string to an array of characters.
+            char[] secretMessage = input.ToCharArray();
+
+
+            //Create a new, empty array of characters to hold the encrypted message. It should be named encryptedMessage and have a length equal to the length of secretMessage.
+
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+
+
+
+
+            //Encrypt
+
+
+            //We’ll need to perform encryption for every letter in the message.
+            //Create an empty for loop that loops through each character of secretMessage
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                //Within the loop, access the character at position i in the secretMessage array and store it in a variable
+                char letter = secretMessage[i];
+
+                //Find the position of the character in the alphabet array using the method Array.IndexOf().Store the value in a variable
+                int letterPosition = Array.IndexOf(alphabet, letter);
+
+
+
+                //Add 3 to the letter position and store the value in a variable
+                //We can “wrap around” by using the modulo operator: %.On the line where you add 3 to the letter position, surround the expression letterPosition + 3 with parentheses and take the modulo of 26.
+                int newLetterPosition = (letterPosition + 3) % 26;
+
+
+
+                //Find the new encrypted character by getting the character in the alphabet array with that new position
+                char letterEncoded = alphabet[newLetterPosition];
+
+
+                //Add the encrypted character to the array encryptedMessage.Store the character at the index i(the iterator variable)
+                encryptedMessage[i] = letterEncoded;
+            }
+
+            //Now we need to convert our array of encrypted characters back into a readable string that we can print to the console
+            string encodedString = String.Join("", encryptedMessage);
+            Console.WriteLine($"your encoded message is : {encodedString}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+//What went wrong? When the program tried to encrypt the z in citizen, it found its index in the alphabet, 25. It looked up the letter 3 spaces to the right, which would be alphabet[28].
+
+//This threw an error because the alphabet is only 26 letters!
+
+//We can “wrap around” by using the modulo operator: %.On the line where you add 3 to the letter position, surround the expression letterPosition + 3 with parentheses and take the modulo of 26.
+
+//Now the new letter position will never go past 26
