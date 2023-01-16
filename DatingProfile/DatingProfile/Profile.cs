e938b022963b57d8b87463a2c1f8f5211dd60ca9
shaisham <102400953+shaisham@users.noreply.github.com>
2023-01-16 18:01:15 +0530

    Created a Dating Profile Project using C#
    With a new understanding of C# objects and classes, I have built a well-organized dating profile system.
    And this is how users should be able to interact with their own profiles:
    
    Created a new profile with some information
    Add hobbies
    View profile

---
+++ DatingProfile/DatingProfile/Profile.cs
@@ -0,0 +1,46 @@
+﻿namespace DatingProfile
+{
+    internal class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronoun;
+        private string[] hobbies;
+
+
+        public Profile(string name, int age, string city = "n/a", string country = "n/a", string pronoun = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronoun = pronoun;
+            this.hobbies = new string[0];
+
+
+        }
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name}\n Age: {age}\n City: {city}\n Country: {country}\n Pronoun: {pronoun} ";
+            if (this.hobbies.Length > 0)
+            {
+                bio += $"\n Hobbies:\n";
+
+                foreach (string hobbie in hobbies)
+                {
+                    bio += $"- {hobbie}\n";
+                }
+            }
+
+            return bio;
+
+        }
+
+        public void SetHobbies(string[] Hobbies)
+        {
+            this.hobbies = Hobbies;
+        }
+    }
+}
---
+++ DatingProfile/DatingProfile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+            sam.SetHobbies(new string[]
+            {
+
+                "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns"
+            });
+
+
+
+            Console.WriteLine(sam.ViewProfile());
+
+
+            Console.ReadLine();
+        }
+    }
+}
